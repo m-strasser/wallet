197f832f675bb117817605700cb9be3b21c6bd2a
m-strasser <mst1409@gmx.at>
2017-04-01 17:05:45 +0200

    Moved argument handling out of main

---
+++ src/arguments.rs
@@ -0,0 +1,100 @@
+/**
+ * Handles the arguments passed to wallet.
+ **/
+use argparse::{ArgumentParser, Store, StoreTrue, Collect};
+use std::fmt;
+
+pub enum ArgsError {
+    NoArgumentsSpecified( String )
+}
+
+impl fmt::Display for ArgsError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            &ArgsError::NoArgumentsSpecified(ref msg) => return write!(f, "{}", msg),
+        };
+    }
+}
+
+/**
+ * Helper struct for arguments that relate to a transaction on an account.
+ **/
+pub struct AccountArgs {
+    pub amount: Option<f64>,
+    pub name: String
+}
+
+impl AccountArgs {
+    pub fn from_string(s: String) -> Result<AccountArgs, ArgsError> {
+        let parts: Vec<&str> = s.split(' ').collect();
+
+        if parts.len() < 1 {
+            return Err(ArgsError::NoArgumentsSpecified(String::from("No arguments given")));
+        }
+
+        let amount:Option<f64> = match parts[0].parse::<f64>() {
+            Ok(v) => Some(v),
+            Err(_) => None
+        };
+
+        match amount {
+            Some(_) => return Ok(
+                AccountArgs {
+                    amount: amount,
+                    name: parts[1..].join(" ")
+                }),
+            None => return Ok(AccountArgs { amount: amount, name: parts.join(" ") })
+        };
+    }
+}
+
+pub struct Args {
+    pub cmd: String,
+    pub account: Option<String>,
+    pub description: String,
+    pub can_overdraw: bool,
+    pub collected: Option<AccountArgs>
+}
+
+pub fn handle_args() -> Args {
+    let mut cmd = String::new();
+    let mut account = String::new();
+    let mut can_overdraw = false;
+    let mut description: String = String::from("No description");
+    let mut str_args: Vec<String> = Vec::new();
+
+    {
+        let mut ap = ArgumentParser::new();
+
+        ap.set_description("FINANCES");
+        ap.refer(&mut cmd)
+            .add_argument("COMMAND", Store, "Command to execute.")
+            .required();
+        ap.refer(&mut str_args)
+            .add_argument("<args>", Collect, "The categories the transaction shall be placed in.");
+        ap.refer(&mut account)
+            .add_option(&["--account", "-a"], Store, "Account to operate on.");
+        ap.refer(&mut description)
+            .add_option(&["-d", "--description"], Store, "Description of the transaction or account.");
+        ap.refer(&mut can_overdraw)
+            .add_option(&["-o", "--overdraw"], StoreTrue, "Pass if the account can be overdrawn.");
+        ap.parse_args_or_exit();
+    }
+
+    let mut arguments = Args {
+        cmd: cmd.clone(), account: None, description: "No description".to_string(),
+        can_overdraw: can_overdraw.clone(), collected: None };
+
+    if account != "" { arguments.account = Some(account.clone()); }
+    if description != "" { arguments.description = description.clone(); }
+    if str_args.len() > 0 {
+        if cmd != "category" {
+            arguments.collected = match AccountArgs::from_string(str_args.join(" ")) {
+                Ok(a) => Some(a),
+                Err(_) => None
+            }
+        }
+    }
+
+    return arguments;
+}
--- src/main.rs
+++ src/main.rs
@@ -3,71 +3,28 @@ mod account;
 mod transaction;
 mod dateutils;
 mod category;
+mod accountmanager;
+mod arguments;
+
 #[cfg(test)]
 mod test_recurring;
-mod accountmanager;
 
 use account::Account;
-use category::Category;
 use accountmanager::print_overview;
 use accountmanager::print_account;
+use arguments::handle_args;
 
 extern crate argparse;
 extern crate chrono;
 
-use argparse::{ArgumentParser, Store, StoreTrue, Collect};
-use std::fmt;
 use std::fs::OpenOptions;
 use std::io::{BufRead, BufReader};
-use chrono::{UTC, TimeZone, Duration};
 
 fn handle_error(msg: String) {
     println!("ERROR: {}!", msg);
 }
 
-enum ArgsError {
-    NoArgumentsSpecified( String )
-}
-
-impl fmt::Display for ArgsError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            &ArgsError::NoArgumentsSpecified(ref msg) => return write!(f, "{}", msg),
-        };
-    }
-}
-
-struct Args {
-    pub amount: Option<f64>,
-    pub name: String
-}
-
-impl Args {
-    fn from_string(s: String) -> Result<Args, ArgsError> {
-        let parts: Vec<&str> = s.split(' ').collect();
-
-        if parts.len() < 1 {
-            return Err(ArgsError::NoArgumentsSpecified(String::from("No arguments given")));
-        }
-
-        let amount:Option<f64> = match parts[0].parse::<f64>() {
-            Ok(v) => Some(v),
-            Err(_) => None
-        };
-
-        match amount {
-            Some(_) => return Ok(
-                Args {
-                    amount: amount,
-                    name: parts[1..].join(" ")
-                }),
-            None => return Ok(Args { amount: amount, name: parts.join(" ") })
-        };
-    }
-}
-
 fn main() {
-    let mut cmd = String::new();
     let mut accounts = Vec::new();
 
     let f = match OpenOptions::new().read(true).open(&account::ACCOUNTS_FILE) {
@@ -89,62 +46,42 @@ fn main() {
     }
 
     let default_account = 0;
-    let mut account = String::new();
-    let mut can_overdraw = false;
     let mut account_index = default_account;
+    let args = handle_args();
     let mut amount: Option<f64> = None;
-    let mut description: String = String::from("No description");
-    let mut str_args: Vec<String> = Vec::new();
-    let mut args: Option<Args> = None;
-    let mut category_flag = -1;
 
-    {
-        let mut ap = ArgumentParser::new();
-        ap.set_description("FINANCES");
-        ap.refer(&mut cmd)
-            .add_argument("COMMAND", Store, "Command to execute.")
-            .required();
-        ap.refer(&mut str_args)
-            .add_argument("<args>", Collect, "The categories the transaction shall be placed in.");
-        ap.refer(&mut account)
-            .add_option(&["--account", "-a"], Store, "Account to operate on.");
-        ap.refer(&mut description)
-            .add_option(&["-d", "--description"], Store, "Description of the transaction or account.");
-        ap.refer(&mut can_overdraw)
-            .add_option(&["-o", "--overdraw"], StoreTrue, "Pass if the account can be overdrawn.");
-        ap.parse_args_or_exit();
-    }
-
-    if accounts.len() == 0 && cmd != "new" {
+    if accounts.len() == 0 && args.cmd != "new" {
         println!("There are no accounts yet.");
         println!("You can create one with the 'new' command.");
         return;
     }
 
-    if str_args.len() > 0 {
-        if cmd != "category" {
-            args = match Args::from_string(str_args.join(" ")) {
-                Ok(a) => {
-                    amount = a.amount;
-                    Some(a)
-                },
-                Err(e) => { if cmd != "show" { handle_error(e.to_string()); return; }; None }
+    match args.collected {
+        Some(ref c) => { amount = (&c.amount).clone(); },
+        None => if args.cmd != "show" {
+            handle_error("You need to specify an amount".to_string());
+            return;
+        }
+    }
+
+    match args.account {
+        Some(a) => {
+            account_index = match accounts.iter().position(|x| x.name == a.clone()) {
+                Some(i) => i,
+                None => { println!("Unkown account, reverting to default!"); default_account }
             };
+        },
+        None => {
+            println!("Unkown account, reverting to default!");
+            account_index = default_account;
         }
     }
 
-    if account != "" {
-        account_index = match accounts.iter().position(|x| x.name == account) {
-            Some(i) => i,
-            None => { println!("Unkown account, reverting to default!"); default_account }
-        }
-    }
-
-    match cmd.as_ref() {
+    match args.cmd.as_ref() {
         "spent" => {
             match amount {
                 Some(a) => {
-                    match accounts[account_index].spent(a, description) {
+                    match accounts[account_index].spent(a, args.description) {
                         Ok(_) => {},
                         Err(e) => { handle_error(e.to_string()); return; }
                     }
@@ -154,7 +91,7 @@ fn main() {
         },
         "got" => {
             match amount {
-                Some(a) => accounts[account_index].got(a, description),
+                Some(a) => accounts[account_index].got(a, args.description),
                 None => {
                     handle_error("Command 'got' requires an amount".to_string());
                     return;
@@ -162,15 +99,15 @@ fn main() {
             };
         },
         "show" => {
-            match args {
+            match args.collected {
                 Some(a) => { print_account(a.name, &accounts); },
                 None => { print_overview(&accounts); }
             }
         },
         "new" => {
-            match args {
+            match args.collected {
                 Some(a) => {
-                    let account: Account = match Account::create(a.name, description, can_overdraw) {
+                    let account: Account = match Account::create(a.name, args.description, args.can_overdraw) {
                         Ok(a) => a,
                         Err(e) => { handle_error(e.to_string()); return; }
                     };
@@ -186,21 +123,21 @@ fn main() {
         "set" => {
           match amount {
             Some(a) => {
-              accounts[account_index].set(a, description);
+              accounts[account_index].set(a, args.description);
             },
             None => { handle_error("Command set needs at least an amount specified".to_string()); return; }
           }
         },
         "category" => {
-            match str_args[0].as_ref() {
-                "add" => {
-                    if str_args.len() < 2 {
-                        handle_error("category add needs at least a name specified".to_string());
-                        return;
-                    }
-                }
-                _ => {}
-            }
+            // match str_args[0].as_ref() {
+            //     "add" => {
+            //         if str_args.len() < 2 {
+            //             handle_error("category add needs at least a name specified".to_string());
+            //             return;
+            //         }
+            //     }
+            //     _ => {}
+            // }
         },
         _ => {
             handle_error("Invalid command supplied".to_string());
